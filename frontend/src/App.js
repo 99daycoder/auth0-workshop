6a9e51c629f5460ca949c6cd36a6d42789b5fb3f
enantes <sambaeverything@gmail.com>
2022-07-21 01:53:15 +0200

    Auth Login / Logout and Profile component , working and tested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
+import LoginButton from "./Components/LoginButton";
+import LogoutButton from "./Components/LogoutButton";
 import Profile from "./Components/Profile";
 function App() {
   return (
     <>
       <h1>React Auth0 Example App</h1>
+      <LoginButton />
+      <LogoutButton />
       <a href="http://localhost:3000/">Home</a> |{" "}
       <a href="http://localhost:3000/profile">My Profile</a> | Other Links..
       <Routes>
